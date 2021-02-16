91eb687a898cdcba68362b0fb6fb6e5e73a1a6b0
Vitalie Paraschiv <vitalieparaskiv@gmail.com>
2021-02-16 18:15:41 +0200

    this is a commit message :)

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const About = () => {
+    return (
+        <div>
+            <h4>Version 1.0.0</h4>
+            <Link to="/">Return to List (Ferencz)</Link>
+        </div>
+    )
+}
+
+export default About
